06369176706aa03f524eee7f6d1545c8c2f0e810
Erik <erik.eriksson@yahoo.com>
2017-02-16 22:28:17 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name='pocketcasts',
+      packages=['pocketcasts'],
+      version='0.0.1',
+      author='',
+      author_email='',
+      url='',
+      download_url='',
+)
