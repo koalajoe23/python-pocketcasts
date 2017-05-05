bd3122d57f9ea4bf943f2c616a2f00b47d59e87c
Tobias Jost <tobias.jost@spinweb.de>
2017-05-05 20:25:27 +0200

    Adapted pypi setup and releasing v0.1. hooray!

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python
-
-from setuptools import setup
-
-setup(name='pocketcasts',
-      packages=['pocketcasts'],
-      version='0.0.1',
-      author='',
-      author_email='',
-      url='',
-      download_url='',
+from distutils.core import setup
+setup(
+  name = 'pocketcasts',
+  packages = ['pocketcasts'], # this must be the same as the name above
+  version = '0.1',
+  description = 'python module to interact with the unofficial Pocket Casts API.',
+  author = 'Tobias Jost',
+  author_email = 'tobias.jost@spinweb.de',
+  url = 'https://github.com/sirl1on/python-pocketcasts', # use the URL to the github repo
+  download_url = 'https://github.com/sirl1on/python-pocketcasts/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['pocketcasts', 'api', 'podcast'], # arbitrary keywords
+  classifiers = [],
 )
